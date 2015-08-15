48b9e449043a841c3711aef30d25b69d5cf7e5c0
CHENXIN LIN <fatesses@gmail.com>
2015-08-15 22:47:07 +0800

    commit

--- js/common.js
+++ js/common.js
@@ -1,23 +1,12 @@
 $(function() {
+	// ordering food
+	var order = function() {
+		alert("Ordering Success");
+	}
+
+	// feedback commit
+	var feedback = function() {
+		alert("Thinks");
+		$.mobile.navigate("#food");
+	}
 });
-
-// ordering food
-var order = function() {
-	alert("Ordering Success");
-}
-
-// feedback commit
-var feedback = function() {
-	alert("Thinks");
-	$.mobile.navigate("#food");
-}
-
-// google map
-// function initialize() {
-//     var mapOptions = {
-//         center: { lat: -34.397, lng: 150.644},
-//         zoom: 8
-//     };
-//     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-// }
-// google.maps.event.addDomListener(window, 'load', initialize);
